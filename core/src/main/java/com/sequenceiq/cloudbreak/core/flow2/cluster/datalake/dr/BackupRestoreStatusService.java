f1dd0f0d1679dd2fe10359672a5faaf8bcfe8b4a
Istvan Rokolya <irokolya@cloudera.com>
2022-05-04 17:30:44 +0200

    CDPSDX-3537: remove invalid state update from database backup

--- core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/cluster/datalake/dr/BackupRestoreStatusService.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/core/flow2/cluster/datalake/dr/BackupRestoreStatusService.java
@@ -43,7 +43,6 @@ public class BackupRestoreStatusService {
     }
 
     public void backupDatabaseFinished(long stackId) {
-        stackUpdater.updateStackStatus(stackId, DetailedStackStatus.DATABASE_BACKUP_IN_PROGRESS, "Database was successfully backed up.");
         flowMessageService.fireEventAndLog(stackId, Status.AVAILABLE.name(), DATALAKE_DATABASE_BACKUP_FINISHED);
     }
 
